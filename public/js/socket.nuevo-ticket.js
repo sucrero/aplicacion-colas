d9f7cbaf4b44c7b5abd263e8dfece0aebdfe0859
Oswaldo Franco <franco.oswaldo@gmail.com>
2018-09-17 13:50:43 -0500

    primer commit

---
+++ public/js/socket.nuevo-ticket.js
@@ -0,0 +1,29 @@
+
+
+// Comando ara establecer la comunicación activo-activo
+
+var socket = io();
+
+var label = $('#lblNuevoTicket')
+
+socket.on('connect', function(){
+    console.log("Conectado al servidor");
+})
+
+socket.on('disconnect', function(){
+    console.log("Desconectado al servidor");
+})
+
+socket.on('estadoActual',function(resp){
+
+    label.text(resp.actual)
+
+})
+
+$('button').on('click', function(){
+    socket.emit('siguienteTicket', null, function(siguienteTicket){
+
+        label.text(siguienteTicket);
+
+    })
+})
